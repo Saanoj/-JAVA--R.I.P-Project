80c6e7f6edd88b60b4cbec92f8f045e0d2c76f47
Tutur77 <38217644+Tutur77@users.noreply.github.com>
2019-05-02 19:41:43 +0200

    push write excel

--- ExcelToBDD/src/excelToBDD/Excel.java
+++ ExcelToBDD/src/excelToBDD/Excel.java
@@ -1,10 +1,16 @@
 package excelToBDD;
 
 import com.mysql.cj.xdevapi.Column;
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -56,7 +62,7 @@ public class Excel {
                                         }
 
                                         if (cell.getColumnIndex() == 12) {
-                                            toBDD(array, newTrajet, chauffeur, priceString,row.getRowNum());
+                                            toBDD(array, newTrajet, chauffeur, priceString,row.getRowNum(),workbook,sheet);
                                         }
 
                     }
@@ -65,28 +71,37 @@ public class Excel {
             }
         }
         workbook.close();
+
+
     }
 
-    private void toBDD(ArrayList<String> array, int newTrajet, String chauffeur, String price,int row) throws SQLException,IOException{
+    private void toBDD(ArrayList<String> array, int newTrajet, String chauffeur, String price,int row,Workbook workbook,Sheet sheet) throws SQLException,IOException{
         if (newTrajet == 1 ){
             Insert.trajet(array);
 
             int id = Select.idTrajet();
             Insert.remuneration(chauffeur,Integer.toString(id),price);
-            changeIdTrajet(id,row);
+            changeIdTrajet(id,row,workbook,sheet);
             System.out.println("L'id nouvelle est "+ id);
         }else{
             Update.trajet(array);
         }
     }
 
-    private void changeIdTrajet(int id, int row) throws IOException{
-        Workbook workbook = WorkbookFactory.create(new File(name));
-        Sheet sheet = workbook.getSheetAt(0);
-        Row headerRow = sheet.createRow(row);
-        Cell cell = headerRow.createCell(0);
-        cell.setCellValue(id);
-        workbook.close();
+    private void changeIdTrajet(int id, int rowId,Workbook workbook,Sheet sheet) throws IOException {
+            Row row = sheet.getRow(rowId);
+            Cell cell = row.getCell(0);
+            if (cell != null) {
+                cell.setCellType(CellType.NUMERIC);
+                cell.setCellValue(id);
+            }
+            FileOutputStream fileOut = new FileOutputStream(name + ".new");
+            workbook.write(fileOut);
+            fileOut.close();
+            Files.delete(Paths.get(name + ".new"));
+
+
+
 
     }
 
