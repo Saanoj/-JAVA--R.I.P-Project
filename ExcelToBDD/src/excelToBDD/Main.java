fe26ced51ee40234ca8d18230cc4f4e39f35ec43
Saanoj <jonasnizard@gmail.com>
2019-05-04 16:42:53 +0200

    embalé c'est claqué

--- ExcelToBDD/src/excelToBDD/Main.java
+++ ExcelToBDD/src/excelToBDD/Main.java
@@ -4,16 +4,10 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException, SQLException {
         Config config = new Config("config.txt");
         Excel excel = new Excel(config.getExcel());
         excel.justDoIt();
-
-
     }
-
-
 }
 
